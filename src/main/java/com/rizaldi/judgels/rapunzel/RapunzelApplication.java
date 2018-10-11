c0f3dd283305e9fe1617394f8289efa7a45b7a05
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-10-11 14:30:09 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/judgels/rapunzel/RapunzelApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.judgels.rapunzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapunzelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapunzelApplication.class, args);
+    }
+}
