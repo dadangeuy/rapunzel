c89e29ab55c82b29afd7e64d041df2e781a735e5
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-11-01 00:48:47 +0700

    add error page

--- src/main/java/com/rizaldi/judgels/rapunzel/controller/ScoreboardController.java
+++ src/main/java/com/rizaldi/judgels/rapunzel/controller/ScoreboardController.java
@@ -9,9 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-
 @Controller
 public class ScoreboardController {
     private final WebConfig webConfig;
@@ -32,17 +29,21 @@ public class ScoreboardController {
     }
 
     @GetMapping("/scoreboard/{contestPath}")
-    public String viewScoreboard(Model model, @PathVariable String contestPath) throws IOException, ExecutionException, InterruptedException {
-        model.addAttribute("logos", webConfig.getLogos());
-        model.addAttribute("icon", webConfig.getIcon());
-        model.addAttribute("title", scoreboardConfig.getTitle(contestPath));
-        model.addAttribute("host", urielConfig.getHost());
+    public String viewScoreboard(Model model, @PathVariable String contestPath) {
+        try {
+            model.addAttribute("logos", webConfig.getLogos());
+            model.addAttribute("icon", webConfig.getIcon());
+            model.addAttribute("title", scoreboardConfig.getTitle(contestPath));
+            model.addAttribute("host", urielConfig.getHost());
 
-        model.addAttribute("aliases", scoreboard.getProblemAlias(contestPath));
-        model.addAttribute("rows", scoreboard.getScoreboardRows(contestPath));
-        model.addAttribute("lastUpdateTime", scoreboard.getLastUpdateTime(contestPath));
+            model.addAttribute("aliases", scoreboard.getProblemAlias(contestPath));
+            model.addAttribute("rows", scoreboard.getScoreboardRows(contestPath));
+            model.addAttribute("lastUpdateTime", scoreboard.getLastUpdateTime(contestPath));
 
-        return "ScoreboardPage";
+            return "ScoreboardPage";
+        } catch (Exception e) {
+            return "ScoreboardErrorPage";
+        }
     }
 
     @GetMapping("/hello")
